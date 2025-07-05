419c0bb0d5781bec91532bbd665221a310ac707d
Florence Haudin <haudin.florence@gmail.com>
2025-07-05 17:11:16 +0200

    Introduce mobile and tablet design for the page.

--- src/components/fundable/index.tsx
+++ src/components/fundable/index.tsx
@@ -13,36 +13,44 @@ export function getCategoryFromProjectPageName(pageName: string) {
   return null;
 }
 
-export default function FundableProjects() {
+export function MainAreaFundableProjects() {
   return (
     <div>
-      <div className="container upper-container-with-margin-top">
-        
-        <div className="row">
-           <div className="col col--2">
-            <MenuSidebar />
-           </div>
-          <div className="col col--9 col--offset-1
-          ">
-            <h1 style={{ padding: "0" }}>Check out our projects available for funding!</h1>
- 
-            <section id="jupyter-ecosystem">
-              <ProjectCategory
-                projectCategoryName={"Jupyter Ecosystem"}
-                projectCategory={fundableProjectsDetails.jupyterEcosystem}
-              />
-            </section>
-            <section id="project-management">
-              <ProjectCategory
-                projectCategoryName={"Package Management"}
-                projectCategory={fundableProjectsDetails.packageManagement}
-              />
-            </section>
-          </div>
+      <h1 style={{ padding: "0" }}>Check out our projects available for funding!</h1>
+
+      <section id="jupyter-ecosystem">
+        <ProjectCategory
+          projectCategoryName={"Jupyter Ecosystem"}
+          projectCategory={fundableProjectsDetails.jupyterEcosystem}
+        />
+      </section>
+      <section id="project-management">
+        <ProjectCategory
+          projectCategoryName={"Package Management"}
+          projectCategory={fundableProjectsDetails.packageManagement}
+        />
+      </section>
+    </div>
+
+  )
+}
+
+export default function FundableProjects() {
+  return (
+
+    <div className="container upper-container-with-margin-top">
+      <div className="row">
+        <div className={"col col--2" + " " + styles.menu_sidebar}>
+          <MenuSidebar />
+        </div>
+        <div className={"col col--9 col--offset-1" + " " + styles.main_area_desktop} >
+          <MainAreaFundableProjects />
+        </div >
+        <div className={"col col--12" + " " + styles.main_area_mobile} >
+          <MainAreaFundableProjects />
         </div>
       </div>
     </div >
-
 
   );
 }
