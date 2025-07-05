1246af655587aa462c82b27f85c4551da1152008
Florence Haudin <haudin.florence@gmail.com>
2025-07-05 13:36:11 +0200

    Update SmallProjectCard to adapt text depending on the current number of funders and the max possible number of them.

--- src/components/fundable/SmallProjectCard.tsx
+++ src/components/fundable/SmallProjectCard.tsx
@@ -1,11 +1,10 @@
 import styles from "./styles.module.css";
-import ProgressBar from "./ProgressBar";
 import { useHistory } from "@docusaurus/router";
-import IconContainer from "./IconContainer";
-import FundersIcon from "@site/static/img/icons/Funders.svg";
-import DollarIcon from "@site/static/img/icons/Dollar.svg";
 import BlueCaretIcon from "@site/static/img/icons/BlueCaret.svg";
 import LinkToMoreInformation from "./LinkToMoreInformation";
+import ProgressBar from "./ProgressBar";
+import FundersIcon from "@site/static/img/icons/Funders.svg";
+import DollarIcon from "@site/static/img/icons/Dollar.svg";
 
 export function SmallProjectCard({ project }) {
   const history = useHistory();
@@ -63,7 +62,7 @@ export function SmallProjectCard({ project }) {
                 </div>
               </div>
               <div className={styles.project_information_container}>
-                 <div className="flex-full-centered">
+                <div className="flex-full-centered">
                   <BlueCaretIcon />
                 </div>
                 <div className={styles.project_information}>
@@ -72,38 +71,47 @@ export function SmallProjectCard({ project }) {
               </div>
 
               <div className={styles.note}>Note: Costs and features can be further adapted following discussion with the funding organization</div>
-
+          </div>
+          <div className={"col col--5"} style={{ backgroundColor: "rgba(217, 217, 217, 0.25" }}>
+            <div className={styles.price_container}>
+              <div className={styles.price}>
+                {project.price}
+              </div>
             </div>
-            <div className={"col col--5"} style={{ backgroundColor: "rgba(217, 217, 217, 0.25" }}>
-              <div className={styles.price_container}>
-                <div className={styles.price}>
-                  {project.price}
-                </div>
-              </div>
-              <div className={styles.indicative_price_text}>
-                Indicative price
-              </div>
+            <div className={styles.indicative_price_text}>
+              Indicative price
+            </div>
+            <div>
+            <div className={styles.financed_at_text}>
+            Financed at {project.currentFundingPercentage} %
+        </div>
+        <div>
+            <ProgressBar value={project.currentFundingPercentage} color={'var(--ifm-color-secondary-s1'} />
+        </div>
+        <div className={styles.shareable_container}>
+            <div><FundersIcon width="35px" height="26px" /></div>
+            <div className={styles.shareable_text}>{project.maxNbOfFunders === 1
+                ? 'Not shareable between funders'
+                : `Shareable between ${project.maxNbOfFunders} funders`
+            }
+            </div>
+        </div>
+        <div className={styles.shareable_container}>
+            <div><DollarIcon width="35px" height="26px" /></div>
+            <div className={styles.shareable_text}>
+                {project.currentNbOfFunders === 0
+                    ? 'The project is not supported yet'
+                    : `Supported by ${project.currentNbOfFunders} 
+                   ${project.currentNbOfFunders === 1 ? 'funder' : 'funders'}
+                    `}
+            </div>
 
-              <div className={styles.financed_at_text}>
-                Financed at {project.currentFundingPercentage} %
-              </div>
-              <div>
-                <ProgressBar value={project.currentFundingPercentage} color={'var(--ifm-color-secondary-s1'} />
-              </div>
-              <div className={styles.shareable_container}>
-                <div><FundersIcon width="35px" height="26px" /></div>
-                <div className={styles.shareable_text}>Shareable between {project.maxNbOfFunders} funder(s).
-                </div>
-              </div>
-              <div className={styles.shareable_container}>
-                <div><DollarIcon width="35px" height="26px" /></div>
-                <div className={styles.shareable_text}>Supported by {project.currentNbOfFunders} funder(s).
-                </div>
-              </div>
+        </div>
             </div>
           </div>
         </div>
       </div>
+    </div>
     </div >
   )
 }
